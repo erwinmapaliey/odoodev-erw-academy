e3af44d25d95dca9f2ed9b4d46e73d8ed1a4d02f
Erwin Mapaliey <erwinmapaliey@gmail.com>
2020-12-08 19:19:03 +0800

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+   'name'        : "Academy",
+   'summary'     : """Custom Module for manage Course in Academy""",
+   'description' : """Custom Module for manage Course in Academy""",
+   'author'      : "Erwin Mapaliey",
+   'website'     : "https://github.com/erwinmapaliey",
+   'category'    : 'Uncategorized',
+   'version'     : '0.1',
+   'depends'     : ['base'],
+   
+   'data': [
+      # 'security/ir.model.access.csv',
+      # 'views/views.xml',
+      # 'views/templates.xml',
+   ],
+}
