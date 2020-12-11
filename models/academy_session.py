c9d5fd4a99b95cacb112a540e626ec9778752d99
Erwin Mapaliey <erwinmapaliey@gmail.com>
2020-12-11 11:47:30 +0800

    Adding Search View and Filter Session

--- models/academy_session.py
+++ models/academy_session.py
@@ -9,6 +9,6 @@ class Session(models.Model):
    course_id     = fields.Many2one( comodel_name='academy.course', string = 'Course' )
    instructor_id = fields.Many2one( comodel_name='res.partner', string = 'Instructor', domain = "[('is_instructor', '=', 'True')]" )
    session_date  = fields.Datetime( string = 'Session Date', default = fields.Datetime.now() )
-   min_attendee  = fields.Integer( string = 'Minimun Attendee' )
+   min_attendee  = fields.Integer( string = 'Minimum Attendee' )
    
    
