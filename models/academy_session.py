df902911fbc85ced74a1abe024f77cb8d661e607
Erwin Mapaliey <erwinmapaliey@gmail.com>
2020-12-11 13:46:58 +0800

    Last Update Model

--- models/academy_session.py
+++ models/academy_session.py
@@ -1,6 +1,5 @@
 from odoo import api, fields, models
 
-
 class Session(models.Model):
    _name        = 'academy.session'
    _description = 'Data Course Session'
@@ -10,14 +9,3 @@ class Session(models.Model):
    instructor_id = fields.Many2one( comodel_name='res.partner', string = 'Instructor', domain = "[('is_instructor', '=', 'True')]" )
    session_date  = fields.Datetime( string = 'Session Date', default = fields.Datetime.now() )
    min_attendee  = fields.Integer( string = 'Minimum Attendee' )
-   attendee_ids  = fields.One2many( comodel_name = 'academy.course.attendee', inverse_name = 'session_id', string = 'Attendee' )
-
-class Attendee(models.Model):
-   _name        = 'academy.course.attendee'
-   _description = 'Attendee of Course Session'
-
-  name          = fields.Char( string = 'Name' )
-  student_id    = fields.Many2one( comodel_name = 'res.partner', string = 'Student', domain = "[('is_student', '=', 'True')]" )
-  register_date = fields.Datetime( string = 'Registration Date', default = fields.Datetime.now() )
-  session_id    = fields.Many2one( comodel_name = 'academy.session', string = 'Session' )
-  remark        = fields.Char( string = 'Remark' )
