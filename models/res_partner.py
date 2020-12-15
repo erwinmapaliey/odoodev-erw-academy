02fa42af8d136b3c2055a4bb6de3fc5f87495023
Erwin Mapaliey <erwinmapaliey@gmail.com>
2020-12-15 12:21:00 +0800

    Update View Course Category dan Edit res.partner

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,9 +1,7 @@
 from odoo import api, fields, models
 
-
 class Partner(models.Model):
    _inherit = 'res.partner'
 
    is_student    = fields.Boolean( string = 'Student', default = False )
-   is_instructor = fields.Boolean( string = 'Instructor', default = False ) 
-   
+   is_instructor = fields.Boolean( string = 'Instructor', default = False )
