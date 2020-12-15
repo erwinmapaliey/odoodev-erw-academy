35061f9d16a6508b80e1c280fddf616fab85c68c
Erwin Mapaliey <erwinmapaliey@gmail.com>
2020-12-15 16:18:14 +0800

    Create New Model - Session Attendee

--- models/academy_session.py
+++ models/academy_session.py
@@ -5,7 +5,15 @@ class Session(models.Model):
    _description = 'Data Course Session'
 
    name          = fields.Char( string = 'Name' )
-   course_id     = fields.Many2one( comodel_name='academy.course', string = 'Course' )
-   instructor_id = fields.Many2one( comodel_name='res.partner', string = 'Instructor', domain = "[('is_instructor', '=', 'True')]" )
-   session_date  = fields.Datetime( string = 'Session Date', default = fields.Datetime.now() )
+   course_id     = fields.Many2one( comodel_name = 'academy.course', string = 'Course' )
+   instructor_id = fields.Many2one( comodel_name = 'res.partner', string = 'Instructor', domain = "[('is_instructor', '=', 'True')]" )
+   session_date  = fields.Datetime( string = 'Session Date', default = fields.Datetime.now(), required = True)
    min_attendee  = fields.Integer( string = 'Minimum Attendee' )
+   description   = fields.Text( string = 'Description' )
+   
+class SessionAttendee(models.Model):
+   _name        = 'academy.session.attendee'
+   _description = 'Course Session Attendee Data'
+
+   name        = fields.Char( string = '' )
+   student_id  = fields.Many2one( comodel_name = 'res.partner', string = 'Student', domain = "[('is_student', '=', 'True')]" )
